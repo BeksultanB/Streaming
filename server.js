f4b241af53c2c6a65cdb6ecda4f12955430325c9
Beks <beksbek3@gmail.com>
2023-10-12 17:13:40 +0600

    Everything is working, but on the site playing not actual videos

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const express = require('express');
+const path = require('path');
 const { exec } = require('child_process');
+
+const app = express();
 const ffmpegPath = "D:\\ffmpeg-6.0-full_build\\bin\\ffmpeg.exe";  // Замените на путь к вашему исполняемому файлу ffmpeg
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const server = app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    startRecording();
+});
+
 function startRecording() {
-    const command = `${ffmpegPath} -rtsp_transport tcp -i rtsp://admin:Microret8@80.20.1.64:554 -codec copy -f segment -segment_time 10 -segment_format mpegts -segment_list public/videos/playlist.m3u8 -segment_list_type m3u8 public/videos/video-%03d.ts`;
-    const process = exec(command, (error, stdout, stderr) => {
+    const command = `${ffmpegPath} -rtsp_transport tcp -i rtsp://admin:Microret8@80.20.1.64:554 -codec copy -f segment -segment_time 1 -segment_format mpegts -segment_list_flags live -segment_list_size 1 -segment_list public/videos/playlist.m3u8 -segment_list_type m3u8 public/videos/video-%03d.ts`;
+    exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing command: ${error.message}`);
             return;
@@ -12,5 +27,3 @@ function startRecording() {
         console.error(`stderr: ${stderr}`);
     });
 }
-
-startRecording();
