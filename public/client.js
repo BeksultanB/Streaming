1304643647a4a436f6f93d0420d1126befa9bae3
Beks <beksbek3@gmail.com>
2023-10-13 20:55:17 +0600

    Implemented WebCodecs techonology

--- public/client.js
+++ public/client.js
@@ -1,1 +1,12 @@
+import JSMpeg from 'https://cdn.jsdelivr.net/npm/jsmpeg-player@3.0.3/+esm'
+console.log(JSMpeg)
 
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+canvas.style.display = "none"
+const player = new JSMpeg.Player('ws://localhost:9999', { canvas: canvas });
+console.log(player)
+const video = document.getElementById('video');
+video.srcObject = canvas.captureStream();
+
+fetch("http://localhost:3000/stream").then(body => console.log(body))
--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-const express = require('express');
+const Stream = require('node-rtsp-stream');
 const path = require('path');
-const { exec } = require('child_process');
-
+const express = require('express');
 const app = express();
-const ffmpegPath = "D:\\ffmpeg-6.0-full_build\\bin\\ffmpeg.exe";  // Замените на путь к вашему исполняемому файлу ffmpeg
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+app.get('/stream', (req, res) => {
+    res.status = "200"
+    res.send()
+});
 const server = app.listen(3000, () => {
     console.log('Server is running on port 3000');
-    startRecording();
 });
-
-function startRecording() {
-    const command = `${ffmpegPath} -rtsp_transport tcp -i rtsp://admin:Microret8@80.20.1.64:554 -codec copy -f segment -segment_time 1 -segment_format mpegts -segment_list_flags live -segment_list_size 1 -segment_list public/videos/playlist.m3u8 -segment_list_type m3u8 public/videos/video-%03d.ts`;
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing command: ${error.message}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-    });
-}
+const stream = new Stream({
+    name: 'name',
+    streamUrl: 'rtsp://admin:Microret8@80.20.1.64:554',
+    wsPort: 9999,
+    ffmpegOptions: {
+        '-stats': '',
+        '-r': 30
+    }
+});
