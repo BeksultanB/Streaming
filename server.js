6062fd40a42e071cb7f3ba681acf0b5f6f617687
Beks <beksbek3@gmail.com>
2023-10-18 18:08:19 +0600

    Streaming with routing

--- server.js
+++ server.js
@@ -1,24 +1,52 @@
 const Stream = require('node-rtsp-stream');
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const app = express();
+let stream = {};
+
+const corsOptions = {
+    origin: '*', // Замените на URL вашего фронтенд-приложения
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+};
+app.use('/', cors(corsOptions));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.get('/stream', (req, res) => {
-    res.status = "200"
-    res.send()
-});
-const server = app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-const stream = new Stream({
-    name: 'name',
-    streamUrl: 'rtsp://admin:Microret8@80.20.1.64:554',
-    wsPort: 9999,
-    ffmpegOptions: {
-        '-stats': '',
-        '-r': 30
+app.get('/start_stream/:cameraId', (req, res) => {
+    const cameraId = req.params.cameraId;
+
+    // Теперь вы можете использовать `cameraId` для создания объекта Stream
+    if (!stream[cameraId]) {
+        stream[cameraId] = new Stream({
+            name: 'name',
+            streamUrl: `rtsp://admin:Microret8@80.20.1.${cameraId}:554`,
+            wsPort: `7${cameraId}`,
+            ffmpegOptions: {
+                '-stats': '',
+                '-r': 30,
+                '-maxrate': '4000k',
+            },
+            http: {
+                port: 8080,
+                mediaroot: './media',
+                allow_origin: '*',
+                bind: '0.0.0.0', // прослушивание всех IP-адресов
+            },
+        });
     }
+    // Далее можно выполнять необходимые действия с объектом `stream`
+    res.send(`Starting stream for camera ${cameraId}`);
+});
+app.get('/stop_stream/:cameraId', (req, res) => {
+    const cameraId = req.params.cameraId;
+    stream[cameraId].stop();
+    delete stream[cameraId]
+    res.send(`Stopped stream for camera ${cameraId}`);
+});
+
+const server = app.listen(81, () => {
+    console.log('Server is running on port 81');
 });
